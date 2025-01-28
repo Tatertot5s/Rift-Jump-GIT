c8fcff9c709e2811630e2fd494d73430a0cc064f
Tatertot5s <100309041+Tatertot5s@users.noreply.github.com>
2025-01-28 09:18:03 -0500

    0.0.5

--- scripts/exit_portal.gd
+++ scripts/exit_portal.gd
@@ -4,5 +4,6 @@ extends StaticBody2D
 @export var LevelName: String = "MainMenu"
 
 func _on_portal_area_body_entered(body):
-	var compiled_level: String = "res://levels/" + LevelName + ".tscn"
-	get_tree().change_scene_to_file(compiled_level)
+	if body.is_in_group("Player"):
+		var compiled_level_name: String = "res://levels/" + LevelName + ".tscn"
+		get_tree().change_scene_to_file(compiled_level_name)
