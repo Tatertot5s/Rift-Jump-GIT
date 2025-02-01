54581db2124fbeee0294407550a1768ce7c4695e
gadge <Tatertot0976@gmail.com>
2025-01-31 20:49:09 -0500

    Revert "0.3.2-unfinished"
    This reverts commit 1a60fd8ee9fcc20f146f6f3651839dfd854ee27a.

--- scripts/multiplayer_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _on_host_pressed():
-	Multiplayer._on_host_pressed()
-	self.visible = !self.visible
-
-func _on_join_pressed():
-	Multiplayer._on_join_pressed()
-	self.visible = !self.visible
