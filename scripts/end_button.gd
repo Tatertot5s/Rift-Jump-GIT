0b6477ff659845298985a29b20308bd47a966767
gadge <Tatertot0976@gmail.com>
2025-01-31 20:49:05 -0500

    Reapply "0.3.1"
    This reverts commit 6da397f62690c45a1d7559d1cf07659a713858ee.

--- scripts/end_button.gd
+++ scripts/end_button.gd
@@ -1,4 +1,8 @@
 extends TextureButton
 
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://levels/MainMenu.tscn")
+
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/MainMenu.tscn")
